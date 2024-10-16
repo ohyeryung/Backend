226c71b4ceff070d61a6e5fe082944cb001fcfae
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-16 21:21:45 +0900

    [Chore] 프로젝트 생성

---
+++ src/main/java/com/manchui/ManchuiApplication.java
@@ -0,0 +1,13 @@
+package com.manchui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManchuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManchuiApplication.class, args);
+	}
+
+}
