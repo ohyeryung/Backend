9e9f2268aa21b8cc284e01903077b86d57037f15
byeunghoon Kang <qudgns119@naver.com>
2024-10-19 20:37:08 +0900

    [Feat] 커스텀 예외 추가

--- src/main/java/com/manchui/global/exception/ErrorCode.java
+++ src/main/java/com/manchui/global/exception/ErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    ILLEGAL_USERNAME_DUPLICATION(HttpStatus.BAD_REQUEST, "이미 가입된 아이디입니다.");
+    ILLEGAL_USERNAME_DUPLICATION(HttpStatus.CONFLICT, "중복된 이름 입니다."),
+    ILLEGAL_EMAIL_DUPLICATION(HttpStatus.CONFLICT, "중복된 이메일 입니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
