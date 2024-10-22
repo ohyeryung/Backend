dfb83ca16b343a7c2bd0c715f761ad99d744c8d1
byeunghoon Kang <qudgns119@naver.com>
2024-10-22 23:22:32 +0900

    [Fix] NameDTO JSON 파싱 문제 해결

--- src/main/java/com/manchui/domain/dto/NameDTO.java
+++ src/main/java/com/manchui/domain/dto/NameDTO.java
@@ -1,10 +1,12 @@
 package com.manchui.domain.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class NameDTO {
 
     private String name;
