661630822842277cb047dab4e6a77e9a224124cb
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-27 21:17:44 +0900

    [Feat] 모임 찾기 기능 구현 (비회원)

--- src/main/java/com/manchui/domain/controller/GatheringController.java
+++ src/main/java/com/manchui/domain/controller/GatheringController.java
@@ -3,6 +3,7 @@ package com.manchui.domain.controller;
 import com.manchui.domain.dto.CustomUserDetails;
 import com.manchui.domain.dto.GatheringCreateRequest;
 import com.manchui.domain.dto.GatheringCreateResponse;
+import com.manchui.domain.dto.GatheringPagingResponse;
 import com.manchui.domain.service.GatheringService;
 import com.manchui.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,12 +13,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,6 +41,23 @@ public class GatheringController {
         return ResponseEntity.status(201)
                 .body(SuccessResponse.successWithData(
                         gatheringService.createGathering(userDetails.getUsername(), createRequest)));
+
+    }
+
+    @Operation(summary = "모임 목록 조회 (비회원)", description = "비회원이 요청하는 모임의 목록을 반환합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "비회원이 요청한 목록이 반환되었습니다.",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "요청 값을 확인해주세요."),
+            @ApiResponse(responseCode = "404", description = "해당하는 모임이 없습니다.")
+    })
+    @GetMapping("/public")
+    public ResponseEntity<SuccessResponse<GatheringPagingResponse>> getGatheringByGuest(@PageableDefault Pageable pageable,
+                                                                                        @RequestParam String query,
+                                                                                        @RequestParam String location,
+                                                                                        @RequestParam String date) {
+
+        return ResponseEntity.ok(SuccessResponse.successWithData(gatheringService.getGatheringByGuest(pageable, query, location, date)));
 
     }
 
