ca19e2829f33303f80ca845f090519730c2fcce4
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-27 21:20:39 +0900

    [Feat] 모임 참석 entity 생성

---
+++ src/main/java/com/manchui/domain/entity/Attendance.java
@@ -0,0 +1,31 @@
+package com.manchui.domain.entity;
+
+import com.manchui.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+
+@Entity
+@Table(name = "attendance")
+@Getter
+@NoArgsConstructor
+public class Attendance extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("참석 id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "gathering_id", nullable = false)
+    @Comment("모임 id")
+    private Gathering gathering;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @Comment("회원 id")
+    private User user;
+
+}
