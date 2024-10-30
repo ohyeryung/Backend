189b1939421de15458e138bee6d8664e7e58b6ff
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-30 17:07:15 +0900

    [Refactor] 모임 좋아요 조회 시 인자 값 수정

--- src/main/java/com/manchui/domain/repository/HeartRepository.java
+++ src/main/java/com/manchui/domain/repository/HeartRepository.java
@@ -1,13 +1,14 @@
 package com.manchui.domain.repository;
 
+import com.manchui.domain.entity.Gathering;
 import com.manchui.domain.entity.Heart;
+import com.manchui.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
-    Optional<Heart> findByUserAndGathering(UUID userId, Long gatheringId);
+    Optional<Heart> findByUserAndGathering(User user, Gathering gathering);
 
 }
