a013a49762c267ea902af649d93ea6371be13f16
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-30 14:31:26 +0900

    [Refactor] 모임 참여 시 발생하는 예외 처리 추가 구현

--- src/main/java/com/manchui/domain/repository/AttendanceRepository.java
+++ src/main/java/com/manchui/domain/repository/AttendanceRepository.java
@@ -1,7 +1,13 @@
 package com.manchui.domain.repository;
 
 import com.manchui.domain.entity.Attendance;
+import com.manchui.domain.entity.Gathering;
+import com.manchui.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    Optional<Attendance> findByUserAndGathering(User user, Gathering gathering);
+
 }
