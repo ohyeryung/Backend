96e7283f8acb7ed77ebb02a7dd4337ef1c5337a9
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-30 19:49:15 +0900

    [Fix] 삭제된 파일 재생성

---
+++ src/main/java/com/manchui/domain/repository/AttendanceRepository.java
@@ -0,0 +1,17 @@
+package com.manchui.domain.repository;
+
+import com.manchui.domain.entity.Attendance;
+import com.manchui.domain.entity.Gathering;
+import com.manchui.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    Optional<Attendance> findByUserAndGathering(User user, Gathering gathering);
+
+    List<Attendance> findByGathering(Gathering gathering);
+
+}
+
