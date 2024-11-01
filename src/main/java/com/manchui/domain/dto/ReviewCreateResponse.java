c6a50700c971a44483eb399ed282045748904431
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-11-02 01:33:56 +0900

    [Feat] Review 요청, 응답 객체 생성

---
+++ src/main/java/com/manchui/domain/dto/ReviewCreateResponse.java
@@ -0,0 +1,20 @@
+package com.manchui.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewCreateResponse {
+
+    private Long gatheringId;
+
+    private int score;
+
+    private String comment;
+
+}
