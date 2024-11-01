2423dd3c97cc31c2feb78e26d982394126ad6db5
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-11-02 01:33:11 +0900

    [Feat] Review Entity 생성

---
+++ src/main/java/com/manchui/domain/entity/Review.java
@@ -0,0 +1,49 @@
+package com.manchui.domain.entity;
+
+import com.manchui.domain.dto.ReviewCreateResponse;
+import com.manchui.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "review")
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("후기 id")
+    private Long id;
+
+    @Column(name = "score", nullable = false)
+    @Comment("후기 평점")
+    private int score;
+
+    @Column(name = "comment", nullable = false)
+    @Comment("후기 내용")
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gathering_id", nullable = false)
+    @Comment("모임 id")
+    private Gathering gathering;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @Comment("회원 id")
+    private User user;
+
+    public ReviewCreateResponse toResponseDto() {
+
+        return ReviewCreateResponse.builder()
+                .gatheringId(gathering.getId())
+                .score(score)
+                .comment(comment)
+                .build();
+    }
+
+}
