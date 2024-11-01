799ad0289c9bc50669f4ca1cf2b30b3bbdb5198e
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-11-01 11:57:27 +0900

    [Refactor] 모임 API 공통 엔드포인트 수정

--- src/main/java/com/manchui/domain/controller/GatheringController.java
+++ src/main/java/com/manchui/domain/controller/GatheringController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/gathering")
+@RequestMapping("/api/gatherings")
 @Slf4j
 public class GatheringController {
 
