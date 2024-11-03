e53765da9afe5db82c4e2ca7bcf2bd7664232001
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-11-03 21:29:32 +0900

    [Refactor] 모임 상세 목록 조회 시 리뷰 리스트 페이징 처리 코드 추가

---
+++ src/main/java/com/manchui/domain/repository/querydsl/ReviewQueryDslImpl.java
@@ -0,0 +1,120 @@
+package com.manchui.domain.repository.querydsl;
+
+import com.manchui.domain.dto.ReviewInfo;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.manchui.domain.entity.QReview.review;
+import static com.manchui.domain.entity.QUser.user;
+
+public class ReviewQueryDslImpl implements ReviewQueryDsl {
+
+    private final JPAQueryFactory queryFactory;
+
+    public ReviewQueryDslImpl(EntityManager em) {
+
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+
+    @Override
+    public Page<ReviewInfo> getReviewInfoList(Pageable pageable, Long gatheringId) {
+
+        List<ReviewInfo> reviewInfpackage com.manchui.domain.repository.querydsl;
+
+import com.manchui.domain.dto.ReviewInfo;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.manchui.domain.entity.QReview.review;
+import static com.manchui.domain.entity.QUser.user;
+
+        public class ReviewQueryDslImpl implements ReviewQueryDsl {
+
+            private final JPAQueryFactory queryFactory;
+
+            public ReviewQueryDslImpl(EntityManager em) {
+
+                this.queryFactory = new JPAQueryFactory(em);
+            }
+
+
+            @Override
+            public Page<ReviewInfo> getReviewInfoList(Pageable pageable, Long gatheringId) {
+
+                List<ReviewInfo> reviewInfoList = queryFactory
+                        .select(
+                                Projections.constructor(
+                                        ReviewInfo.class,
+                                        user.name,
+                                        user.profileImagePath,
+                                        review.score,
+                                        review.comment,
+                                        review.createdAt
+                                )
+                        )
+                        .from(review)
+                        .leftJoin(review.user, user)
+                        .where(review.gathering.id.eq(gatheringId))
+                        .limit(pageable.getPageSize())
+                        .offset(pageable.getOffset())
+                        .fetch();
+
+                long total = Optional.ofNullable(
+                        queryFactory
+                                .select(review.count())
+                                .from(review)
+                                .where(review.gathering.id.eq(gatheringId))
+                                .fetchOne()
+                ).orElse(0L);
+
+                return new PageImpl<>(reviewInfoList, pageable, total);
+            }
+
+
+        }
+        oList = queryFactory
+                .select(
+                        Projections.constructor(
+                                ReviewInfo.class,
+                                user.name,
+                                user.profileImagePath,
+                                review.score,
+                                review.comment,
+                                review.createdAt
+                        )
+                )
+                .from(review)
+                .leftJoin(review.user, user)
+                .where(review.gathering.id.eq(gatheringId))
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .fetch();
+
+        long total = Optional.ofNullable(
+                queryFactory
+                        .select(review.count())
+                        .from(review)
+                        .where(review.gathering.id.eq(gatheringId))
+                        .fetchOne()
+        ).orElse(0L);
+
+        return new PageImpl<>(reviewInfoList, pageable, total);
+    }
+
+
+}
