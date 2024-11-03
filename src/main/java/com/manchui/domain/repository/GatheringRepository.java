e4eb489864d69e18c7e095949f6354e0335b983c
byeunghoon Kang <qudgns119@naver.com>
2024-11-03 18:47:57 +0900

    [Feat] 모임 ID 목록을 통한 페이징 조회 메서드 추가

--- src/main/java/com/manchui/domain/repository/GatheringRepository.java
+++ src/main/java/com/manchui/domain/repository/GatheringRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GatheringRepository extends JpaRepository<Gathering, Long>, GatheringQueryDsl {
 
     Page<Gathering> findByUserEquals(User user, Pageable pageable);
+
+    Page<Gathering> findByIdIn(List<Long> gatheringIdList, Pageable pageable);
 }
