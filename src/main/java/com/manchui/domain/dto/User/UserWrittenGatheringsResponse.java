30b0b01d9c270dcbc75994d5d1ae10174b7d2562
byeunghoon Kang <qudgns119@naver.com>
2024-11-03 18:46:08 +0900

    [Refactor] DTO명 변경에 따른 반환 타입명 수정

--- src/main/java/com/manchui/domain/dto/User/UserWrittenGatheringsResponse.java
+++ src/main/java/com/manchui/domain/dto/User/UserWrittenGatheringsResponse.java
@@ -4,19 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class UserWrittenGatheringsResponse {
 
     private long gatheringCount;
-    private Page<WrittenGathering> writtenGatheringList;
+    private Page<GatheringInfo> writtenGatheringList;
     private long pageSize;
     private long page;
     private long totalPage;
 
-    public UserWrittenGatheringsResponse(long gatheringCount, Page<WrittenGathering> writtenGatheringList, long pageSize, long page, long totalPage) {
+    public UserWrittenGatheringsResponse(long gatheringCount, Page<GatheringInfo> writtenGatheringList, long pageSize, long page, long totalPage) {
         this.gatheringCount = gatheringCount;
         this.writtenGatheringList = writtenGatheringList;
         this.pageSize = pageSize;
