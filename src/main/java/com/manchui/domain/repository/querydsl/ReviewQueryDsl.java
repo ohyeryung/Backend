1bb4d59c3421e01ee2d1f07abc0627f6d289bad4
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-11-05 15:07:02 +0900

    [Feat] 후기 목록 조회 시 querydsl을 활용한 페이징 처리 및 필터링 구현

--- src/main/java/com/manchui/domain/repository/querydsl/ReviewQueryDsl.java
+++ src/main/java/com/manchui/domain/repository/querydsl/ReviewQueryDsl.java
@@ -1,5 +1,6 @@
 package com.manchui.domain.repository.querydsl;
 
+import com.manchui.domain.dto.review.ReviewDetailInfo;
 import com.manchui.domain.dto.review.ReviewInfo;
 import com.manchui.domain.dto.review.ReviewScoreInfo;
 import org.springframework.data.domain.Page;
@@ -7,8 +8,12 @@ import org.springframework.data.domain.Pageable;
 
 public interface ReviewQueryDsl {
 
-    ReviewScoreInfo getScoreStatistics(Long gatheringId);
+    ReviewScoreInfo getScoreStatisticsByGathering(Long gatheringId);
+
+    ReviewScoreInfo getScoreStatistics(String query, String location, String category, String startDate, String endDate);
 
     Page<ReviewInfo> getReviewInfoList(Pageable pageable, Long gatheringId);
+
+    Page<ReviewDetailInfo> getReviewDetailInfo(Pageable pageable, String query, String location, String startDate, String endDate, String category, String sort);
 
 }
